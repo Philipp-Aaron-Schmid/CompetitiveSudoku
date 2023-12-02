ac57e0f97dac84a3cb33f03a34679590e0596ecd
Philipp Schmid <ph.a.schmid@gmail.com>
2023-12-02 10:22:16 +0100

    SpringInitializr Backend added to the Project

---
+++ Backend/m223/m223/src/main/java/ch/wiss/m223/M223Application.java
@@ -0,0 +1,13 @@
+package ch.wiss.m223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M223Application.class, args);
+	}
+
+}
